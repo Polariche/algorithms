00bb21c5f80e0c7d7a817dc736e5de01e05cdcf7
Polariche <polariche@gmail.com>
2023-11-25 19:08:05 +0900

    solve beads

--- USACO/USACO_Training/Ch1/beads.cpp
+++ USACO/USACO_Training/Ch1/beads.cpp
@@ -10,8 +10,9 @@ LANG: C++
 using namespace std;
 
 #define NEW_ARRAY(N) (int*)malloc(sizeof(int)*N);
-#define COMP(a,b) ((bool) (a&b))
+#define SAME(a,b) ((bool) (a&b))
 #define MAX(a,b) (a>b?a:b)
+#define MIN(a,b) (a<b?a:b)
 
 int main() {
     ofstream fout ("beads.out");
@@ -20,61 +21,73 @@ int main() {
     int N;
     string str;
     int *bead;
-    int *cnt[4];
+    int *start[4];
+    int *end[4];
     int best = 0, chain=0, last_chain=0;
-    bool flag=true;
-
+    int flag = 0;
     fin >> N >> str;
     
     bead = NEW_ARRAY(N);
 
     for (int i=0;i<N;i++) {
         switch(str[i]) {
-            case 'r': bead[i] = 1; break;
-            case 'b': bead[i] = 2; break;
+            case 'r': bead[i] = 1; flag |= 1; break;
+            case 'b': bead[i] = 2; flag |= 2; break;
             case 'w': bead[i] = 3; break;
         }
     }
 
-    for (int i=0;i<4;i++) {
-        cnt[i] = NEW_ARRAY(N);
+    if (flag < 3) { 
+        // single-colored
+        fout << N << endl;
+        return 0;
+    } 
+
+    for (int i=1;i<3;i++) {
+        start[i] = NEW_ARRAY(N);
+        end[i] = NEW_ARRAY(N);
     }
     
     for (int k=0;k<2;k++) {
         for (int i=0;i<N;i++) {
             int j = (i-1+N)%N;
-            int cur;
             int a = bead[i], b = bead[j];
 
-            for (int k=1;k<=2;k++) {
-                cnt[k][i] = COMP(a,k) + (COMP(a,b) ? cnt[k][j] : 0);
+            for (int c=1;c<=2;c++) {
+                if (!SAME(a, c)) {
+                    // chain broken
+                    start[c][i] = end[c][i] = -1;
+                    end[c][start[c][j]] = j;
+
+                } else if (SAME(a, b) && start[c][j] > -1) {
+                    // chain continued
+                    start[c][i] = start[c][j];
+                    end[c][i] = -1;
+
+                } else {
+                    // new chain
+                    start[c][i] = i;
+                    if (!end[c][i]) end[c][i] = i;
+                }
             }
-
-            if (a == 3) cnt[3][i] = cnt[1][i] > cnt[2][i] ? 1 : 2;
-            else    cnt[3][i] = a;
-
-            if (cnt[3][i] != cnt[3][j] && a != b) {
-                int t = cnt[3][j];
-                flag = false;
-                best = MAX(last_chain+cnt[t][j] - (b==3), best);
-
-                cnt[t][i] = 0;
-
-                cout << "chain cut: " << best << endl;
-
-                last_chain = cnt[t][j];
-            }
-
-            cout << cnt[1][i] << " " << cnt[2][i] << " " << best << endl;
         }
-        cout << "break" << endl;
     }
 
-    if (flag) {
-        best = N;
+    for (int i=0;i<N;i++) {
+        for (int c=1;c<=2;c++) {
+            int d = (~c)&3;
+            if (end[c][i] > -1) {
+                // suppose we break the beads at end[c][i]
+                int j = (end[c][i]+1)%N;
+                int cnt = end[d][start[d][j]] - i + 1;
+                if (cnt < 0) cnt += N;
+                best = MAX(best, cnt);
+            }
+        }
     }
-    
+
     fout << best << endl;
 
+    
     return 0;
 }
