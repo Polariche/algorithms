9f9891e398ab325308ec4d58c816d9af24b61f76
Polariche <polariche@gmail.com>
2023-12-01 23:48:20 +0900

    ariprog WIP

--- USACO/USACO_Training/Ch1/ariprog.cpp
+++ USACO/USACO_Training/Ch1/ariprog.cpp
@@ -20,7 +20,39 @@ int main() {
     ofstream cout ("ariprog.out");
     ifstream cin ("ariprog.in");
 
-    
+    int N,M;
+    cin >> N >> M;
+
+    int bi[M*M*4+2]={0};
+    set<int> biset;
+
+    for (int p=0;p<=M;p++) {
+        for (int q=0;q<=M;q++) {
+            bi[p*p+q*q]=1;
+            biset.insert(p*p+q*q);
+        }
+    }
+
+    // O(M^4) lmao
+    int total=0;
+    for(int b=1;b<=M*M;b++) {
+        set<int>::iterator it;
+
+        for (it = biset.begin(); it != biset.end();it++) {
+            int i;
+            int a = *it;
+            for (i=0; (i<N && bi[a+i*b]); i++);
+            
+            if (i==N) {
+                cout << a << " " << b << endl;
+                total++;
+                if (total>10000)    return 0;
+            }
+        }
+    }
+
+    if (total == 0)
+        cout << "NONE" << endl;
 
     return 0;
 }
