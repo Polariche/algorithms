796d9647ae00ff5fa3bf16ee837d266a2c7eafe1
Polariche <polariche@gmail.com>
2023-12-22 20:01:52 +0900

    solve money

--- USACO/USACO_Training/Ch2/money.cpp
+++ USACO/USACO_Training/Ch2/money.cpp
@@ -24,29 +24,19 @@ typedef unsigned long long ull;
 typedef pair<int,int> pi;
 
 #define M 26
+int coins[M];
+ll dp[M][10001];
 
-void deduct(int v, int n, int m[M], int cnt[M]) {
-    for (int i=v;i>0;i--) {
-        cnt[i] = n/m[i];
-        n %= m[i];
-    }
-}
+ll solve(int v, int n) {
+    if(!v || !n) return dp[v][n]=!n;
+    if (dp[v][n] > -1)  return dp[v][n];
+    
+    dp[v][n] = 0;
 
-int solve(int i, int cnt[M], int vcnt[M][M]) {
-    if (i==0)   return 0;
-    else if (i==1) {for(int j=1;j<=3;j++) cout << cnt[j] << " "; cout << endl; return 1;}
+    for (int i=n/coins[v];i>=0;i--)
+        dp[v][n] += solve(v-1, n-coins[v]*i);
 
-    int a = 0;
-
-    int original[M];
-    copy(cnt, cnt+M, original);
-    for (;cnt[i]>=0;cnt[i]--) {
-        a += solve(i-1, cnt, vcnt);
-        for (int j=i-1;j>0;j--)
-            cnt[j] += vcnt[i][j];
-    }
-    copy(original, original+M, cnt);
-    return a;
+    return dp[v][n];
 }
 
 int main() {
@@ -54,30 +44,17 @@ int main() {
     ifstream cin ("money.in");
 
     int v,n;
-    int m[M], cnt[M], vcnt[M][M];
-    map<int,int> rep;
+    
     cin >> v >> n;
-
     for (int i=1;i<=v;i++) {
-        cin >> m[i];
+        cin >> coins[i];
+        for (int j=1;j<=n;j++) {
+            dp[i][j] = -1;
+        }
     }
         
-
-    sort(m, m+v);
-
-    deduct(v, n, m, cnt);
-    for (int i=v;i>0;i--)
-        deduct(i-1, m[i], m, vcnt[i]);
-
-    for (int i=v;i>0;i--) {
-        cout << cnt[i];
-        for (int j=i-1;j>0;j--) {
-            cout << " " << vcnt[i][j];
-        }
-        cout << endl;
-    }
-
-    cout << solve(v, cnt, vcnt) << endl;
+    sort(coins, coins+v);
+    cout << solve(v,n) << endl;
 
     return 0;
 }
